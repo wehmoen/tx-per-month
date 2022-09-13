5f5d76b550c57d3b8fc4c0ac43499fa4f9d7915d
Nico <mail@wehmoen.dev>
2022-09-13 20:18:56 +0200

    Try to export statistics using $and expression

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,72 @@
 use std::collections::HashMap;
-use serde::{Serialize, Deserialize};
-use  futures::Stream;
-use futures::stream::{StreamExt};
 use mongodb::bson::{DateTime, doc};
-use mongodb::options::FindOptions;
+use serde::{Deserialize, Serialize};
 
-type Year = u64;
+type Year = i64;
 
-#[derive(Default, Serialize, Deserialize)]
-struct RoninChainStatistics {
-    january: u64,
-    february: u64,
-    march: u64,
-    april: u64,
-    may: u64,
-    june: u64,
-    july: u64,
-    august: u64,
-    september: u64,
-    october: u64,
-    november: u64,
-    december: u64,
+type RoninChainStatistics = [i64; 12];
+
+fn create_ronin_chain_statistics() -> RoninChainStatistics {
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 }
 
 #[derive(Serialize, Deserialize)]
 struct RoninTransaction {
-    created_at: DateTime
+    created_at: DateTime,
 }
 
 #[tokio::main]
 async fn main() {
-
     let client = mongodb::Client::with_uri_str("mongodb://localhost:27017").await.expect("Failed to create database connection!");
     let database = client.database("ronin");
     let collection = database.collection::<RoninTransaction>("transactions");
 
-    let mut statistics : HashMap<Year, RoninTransaction> = HashMap::new();
+    let mut statistics: HashMap<Year, RoninChainStatistics> = HashMap::new();
 
-    let options = FindOptions::builder()
-        .no_cursor_timeout(Some(true))
-        .batch_size(Some(100u32))
-        .sort(doc! {
-                "created_at": 1i64
-            })
-        .limit(5)
-        .build();
+    let years: [Year; 3] = [2020, 2021, 2022];
 
-    let mut cursor = collection.find(None, options).await.unwrap();
+    let months: Vec<i64> = (1..=12).collect();
 
-    while let Some(tx) = cursor.next().await {
-            let tx = tx.unwrap();
-            let date_parts = tx.created_at.to_string();
+    for year in years {
+        let mut this_year = create_ronin_chain_statistics();
+        for month in months.iter() {
+            println!("Year: {:<5}Month: {:<2}", year, month);
+            let index = (month - 1) as usize;
 
-        println!("{}", date_parts);
+            let res = collection.count_documents(doc! {
+               "$and": [
+                    {
+                        "$expr": {
+                            "$eq": [
+                                {
+                                    "$year": "$created_at"
+                            },
+                            year
+                            ]
+                        }
+                    },
+                    {
+                        "$expr": {
+                            "$eq": [
+                                {
+                                    "$month": "$created_at"
+                            },
+                            month
+                            ]
+                        }
+                    },
 
+                ]
+
+            }, None).await.unwrap();
+
+            this_year[index] = res as i64;
+        }
+
+        statistics.insert(year, this_year);
     }
 
+    std::fs::write("tx-per-month.json", serde_json::to_string(&statistics).unwrap()).unwrap();
 
-    println!("Export!");
+    println!("Results stored as: tx-per-month.json");
 }
